6e030054edd44b17d1fc11d177761925de677220
Morris <tappan.billz@gmail.com>
2020-02-18 22:06:24 +0800

    add tcp client and tcp server  chapter2 lesson 24-26

---
+++ 百万网络并发/EasyTcpServer/EasyTcpClient/client.cpp
@@ -0,0 +1,49 @@
+﻿#define WIN32_LEAN_AND_MEAN  // 避免早期定义的宏冲突
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <windows.h>
+#include <WinSock2.h>
+#include <iostream>
+using namespace std;
+
+#pragma comment(lib,"ws2_32.lib")
+
+
+int main()
+{
+	// 启动socket 网络环境
+	WORD version = MAKEWORD(2, 2);
+	WSADATA dat;
+	WSAStartup(version, &dat);
+
+	// 建立一个TCP客户端
+	// 1.建立一个socket
+	SOCKET _sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (INVALID_SOCKET == _sock)
+	{
+		cout << "socket error" << endl;
+	}
+	// 2.连接服务器
+	sockaddr_in _sin = {};
+	_sin.sin_family = AF_INET;
+	_sin.sin_port = htons(4567);
+	_sin.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+	if (SOCKET_ERROR ==  connect(_sock, (sockaddr*)&_sin, sizeof(sockaddr)))
+	{
+		cout << "connect error" << endl;
+	}
+
+	// 3.接受服务器发送的信息
+	char recvbuf[256] = {};
+	int nlen = recv(_sock, recvbuf, 256, 0);
+	if (nlen > 0)
+	{
+		cout << "接收到数据:" << recvbuf << endl;
+	}
+
+	// 4.关闭套接字
+	closesocket(_sock);
+	WSACleanup();
+
+	getchar();
+	return 0;
+}
---
+++ 百万网络并发/EasyTcpServer/EasyTcpServer/server.cpp
@@ -0,0 +1,61 @@
+﻿#define WIN32_LEAN_AND_MEAN  // 避免早期定义的宏冲突
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <windows.h>
+#include <WinSock2.h>
+#include <iostream>
+using namespace std;
+
+#pragma comment(lib,"ws2_32.lib")
+
+
+int main()
+{
+	// 启动socket 网络环境
+	WORD version = MAKEWORD(2, 2);
+	WSADATA dat;
+	WSAStartup(version, &dat);
+
+	// 建立一个TCP服务器
+	// 1.建立一个socket
+	SOCKET _sock = socket(AF_INET/*IPV4*/, SOCK_STREAM/*数据流*/, IPPROTO_TCP);
+	// 2.bind 绑定用于接受客户端的网络端口
+	sockaddr_in _sin = {};
+	_sin.sin_family = AF_INET;
+	_sin.sin_port = htons(4567);
+	_sin.sin_addr.S_un.S_addr = INADDR_ANY; //inet_addr("127.0.0.1");
+	int bs = bind(_sock, (sockaddr*)&_sin, sizeof(_sin));
+	if (SOCKET_ERROR == bs)
+	{
+		cout << "Error 绑定错误" << endl;
+	}
+	// 3.监听网络端口
+	if (SOCKET_ERROR == listen(_sock, 5))
+	{
+		cout << "Error 监听失败" << endl;
+	}
+
+	// 4.等待客户端连接
+	sockaddr_in client_addr = {};
+	SOCKET _csock = INVALID_SOCKET;
+	int n_addr = sizeof(sockaddr_in);
+
+	char buffer[] = "Hello, I'm Server.";
+	while (true)
+	{
+		_csock = accept(_sock, (sockaddr*)&client_addr, &n_addr);
+		if (INVALID_SOCKET == _sock)
+		{
+			cout << "Error 无效的客户端socket" << endl;
+		}
+
+		// 5.发送消息
+		cout <<"新客户端IP:" << inet_ntoa(client_addr.sin_addr)<< endl;
+		send(_csock, buffer, strlen(buffer) + 1, 0);
+	}
+	
+
+	// 6.关闭套接字
+	closesocket(_sock);
+	WSACleanup();
+	return 0;
+}
