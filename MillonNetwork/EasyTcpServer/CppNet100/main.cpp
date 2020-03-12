ee26dc9cc395790a7e66effcf3f67b501bf9a444
Morris <tappan.billz@gmail.com>
2020-03-12 20:54:16 +0800

    lesson 228 创建动态库

---
+++ MillonNetwork/EasyTcpServer/CppNet100/main.cpp
@@ -0,0 +1,22 @@
+﻿
+#ifndef _CPP_NET_100_DLL_H_
+#define _CPP_NET_100_DLL_H_
+
+
+
+extern "C" // 请按照C的方式导出
+{
+	int _declspec(dllexport)/*导出C++函数*/ add(int a, int b)
+	{
+		return a + b;
+	}
+
+
+}
+
+
+
+
+
+
+#endif   // !_CPP_NET_100_DLL_H_
