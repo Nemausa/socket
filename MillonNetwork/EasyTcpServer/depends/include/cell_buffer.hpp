aa696d0b69e3114788a8f19adb4f8b10e495f32d
Morris <tappan.billz@gmail.com>
2020-03-15 14:33:47 +0800

    CellBuffer 发送数据存在bug，可能一条数据需要多次才能发完的情况

--- MillonNetwork/EasyTcpServer/depends/include/cell_buffer.hpp
+++ MillonNetwork/EasyTcpServer/depends/include/cell_buffer.hpp
@@ -23,7 +23,7 @@ public:
 	{
 		size_ = size;
 		buffer_ = new char[size_];
-		last_ = 0;
+		length_ = 0;
 		full_count_ = 0;
 	}
 	~CellBuffer() 
@@ -52,13 +52,13 @@ public:
 		//	delete[] buffer_;
 		//	buffer_ = buff;
 		//}
-		if (last_ + length <= size_)
+		if (length_ + length <= size_)
 		{
 			// 将要发送的数据拷贝到发送缓冲区尾部
-			memcpy(buffer_ + last_, pdata, length);
+			memcpy(buffer_ + length_, pdata, length);
 			// 数据尾部位置
-			last_ += length;
-			if (last_ == size_)
+			length_ += length;
+			if (length_ == size_)
 				full_count_++;
 			return true;
 		}
@@ -71,12 +71,12 @@ public:
 
 	void pop(int length)
 	{
-		int n = last_ - length;
+		int n = length_ - length;
 		if (n > 0)
 		{
 			memcpy(buffer_, buffer_ + length, n);
 		}
-		last_ = n;
+		length_ = n;
 		if (full_count_ > 0)
 			--full_count_;
 
@@ -86,10 +86,23 @@ public:
 	int write_socket(SOCKET sockfd)
 	{
 		int ret = 0;
-		if (last_ > 0)
+		if (length_ > 0)
 		{
-			ret = send(sockfd, buffer_, last_, 0);
-			last_ = 0;
+			ret = send(sockfd, buffer_, length_, 0);
+			if(ret<=0)
+			{
+				return SOCKET_ERROR;
+			}
+			else if (ret == length_)
+			{
+				// 数据尾部清零
+				length_ = 0;
+			}
+			else
+			{
+				length_ -= ret;
+				memcpy(buffer_, buffer_ + ret, length_);
+			}
 			full_count_ = 0;
 		}
 		return ret;
@@ -98,41 +111,41 @@ public:
 	int read_socket(SOCKET sockfd)
 	{
 		
-		if (size_ - last_ < 0)
+		if (size_ - length_ < 0)
 			return 0;
-		char *recv_buf = buffer_ + last_;
-		int len = (int)recv(sockfd, recv_buf, size_ - last_, 0);
+		char *recv_buf = buffer_ + length_;
+		int len = (int)recv(sockfd, recv_buf, size_ - length_, 0);
 		if (len <= 0)
 		{
 
 			return len;
 		}
-		last_ += len;
+		length_ += len;
 		return  len;
 	}
 
 	bool has_msg()
 	{
-		if (last_ >= sizeof(NetDataHeader))
+		if (length_ >= sizeof(NetDataHeader))
 		{
 			// 这时候就知道了当前消息的长度
 			NetDataHeader *head = (NetDataHeader*)buffer_;
 			// 判断消息缓冲区的数据长度大于消息长度
-			return (last_ >= head->length_);
+			return (length_ >= head->length_);
 		}
 		return false;
 	}
 	
 	bool need_write()
 	{
-		return last_;
+		return length_;
 	}
 
 private:
 	// 可以用链表和队列管理缓存
 	//list<char*> list_buffer_;
 	char* buffer_;  
-	int last_;		// 缓冲区尾部位置
+	int length_;	// 缓冲区实际数据的长度
 	int size_;		// 缓冲区字节长度
 	int full_count_;
 };
